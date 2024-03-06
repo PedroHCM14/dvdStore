d1b8b42da1a2f5edd071a5108a8abe0212e2b2c2
Pedro Martins <pedrohenrique.251@hotmail.com>
2024-03-05 22:56:48 -0300

    Adição NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return
+  <nav>
+    <a href="#">Home</a>
+    <a href="#">DVDs</a>
+    <a href="#">Blu-Rays</a>
+    <a href="#">Lançamentos</a>
+  </nav>;
+}
+
+export default NavBar;
